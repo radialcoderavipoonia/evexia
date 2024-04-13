13f0e5ac556c024ea657dee15de0634b8dc042d7
radialcoderavipoonia <ravipoonia28122002@gmail.com>
2024-04-13 12:27:36 +0530

    font family added

---
+++ src/components/OurBenefits.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const OurBenefits = () => {
+  return (
+    <div className="xl:max-w-[1140px] xl:px-0 px-3 container py-[120px] mx-auto">
+      <div className="flex justify-center">
+        <span className="text-center bg-[#E5F4EF] text-[#27AA7E] border-[1px] border-[#27AA7E] px-[30px] py-[10px] text-base font-semibold font-OpenSans leading-[24px] rounded-[51px]">
+          OUR BENEFITS
+        </span>
+      </div>
+      <h2 className="pt-[6px] text-center text-[48px] font-bold font-Roboto leading-[57.6px] text-black">
+        Evexia Benefits: Tailored <span className="text-[#27AA7E]">Wellness Unlocked</span>
+      </h2>
+    </div>
+  );
+};
+
+export default OurBenefits;
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        Roboto: ["Roboto", "sans - serif"],
+        OpenSans: ["Open Sans", "sans-serif"],
+        NotoSans: ["Noto Sans", "sans-serif"],
+      },
     },
   },
   plugins: [],
